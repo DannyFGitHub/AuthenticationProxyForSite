66c0d1be684c2d2c329de37abe458b0c73383f38
ObsidianDwarf <obsidiandwarf@ObsidianDwarfs-iMac.local>
2021-07-04 04:55:38 +1000

    Added back https

--- index.js
+++ index.js
@@ -5,19 +5,36 @@ const bodyParser = require('body-parser');
 const Protection = require('./Protection.js');
 const morgan = require("morgan");
 const path = require('path');
+const https = require('https');
 const fs = require('fs');
 const rfs = require('rotating-file-stream');
 const Repository = require('./Repository.js');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const Whitelist = require('./Whitelist.js');
+const helmet = require("helmet");
+
 (require('dotenv')).config();
 
+let key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
+let cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
+let options = {
+  key: key,
+  cert: cert
+};
+
 const app = express();
+app.use(helmet({
+    contentSecurityPolicy: false,
+    noSniff: false
+}));
+
+let server = https.createServer(options, app);
 
 //Proxy Related
 const PORT = process.env.AUTH_PORT;
 const HOST = process.env.AUTH_HOST;
 const PROGRAM_SERVICE_URL = process.env.PROGRAM_SERVICE_URL;
+
 
 /////// LOGIN
 
@@ -220,7 +237,11 @@ app.use((err, req, res, next) => {
     next();
 });
 
-// Start the Proxy
-app.listen(PORT, HOST, () => {
+// // Start the Proxy
+// app.listen(PORT, HOST, () => {
+//     console.log(`Booting ${HOST}:${PORT}`);
+// });
+
+server.listen(PORT, HOST, () => {
     console.log(`Booting ${HOST}:${PORT}`);
 });
